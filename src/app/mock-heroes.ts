d6de6a23551a07815a42b0a6390a095db488e227
rudycito23 <rrlucas@live.com>
2024-01-23 18:23:20 -0600

    Add comments to mock-heroes.ts

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,5 +1,7 @@
+// import the Hero interface object which contains two properties name & id
 import { Hero } from "./hero";
 
+//  exporting a constant named HEROES; this constant is an array of Hero objects
 export const HEROES: Hero[] = [
   { id: 12, name: ' Wolverine' },
   { id: 13, name: ' Nightcrawler' },
