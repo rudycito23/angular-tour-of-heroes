9bf4ebbff58a623f79eb2d13bb0cf203249046e4
rudycito23 <rrlucas@live.com>
2024-01-23 18:22:46 -0600

    Display a list of heroes with a detail view; the user can select a hero and see the that hero's details; used *ngFor to display a list; used *ngIf to conditionally include/exclude a block of HTML; toggle a CSS style class with a class binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,24 +3,36 @@
     --skip-import flag is required to skip importing NgModule */
 }
 
-{/*  */}
+{
+  /*  */
+}
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // provides common directive, in this case pipes
+import { CommonModule, NgFor } from '@angular/common'; // provides common directive, in this case pipes
 import { FormsModule } from '@angular/forms'; // provides support for template-driven forms: ngModel
 import { Hero } from '../hero'; // importing the Hero interface from hero.ts
+import { HEROES } from '../mock-heroes';
 
 // Component decorator providing metadata for the HeroesComponent
 @Component({
   selector: 'app-heroes', // defines the custom HTML tag: <app-heroes></app-heroes>
   standalone: true, // standalone component and can be used without NgModule
-  imports: [CommonModule, FormsModule], // imports necessary for this component
+  imports: [CommonModule, FormsModule, NgFor], // imports necessary for this component
   templateUrl: './heroes.component.html', // path to HTML template
-  styleUrls: ['./heroes.component.css'],  // path to CSS styles
+  styleUrls: ['./heroes.component.css'], // path to CSS styles
 })
 export class HeroesComponent {
   // property hero of type Hero with initial values
-  hero: Hero = {
+  /*hero: Hero = {
     id: 1,
     name: 'Spider-Man' // default hero name
   };
+*/
+
+  //  expose the HEROES array for binding
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: ' Wolverine' },
+  { id: 13, name: ' Nightcrawler' },
+  { id: 14, name: ' Colossus' },
+  { id: 15, name: ' Gambit' },
+  { id: 16, name: ' Captain America' },
+  { id: 17, name: ' Iron Man' },
+  { id: 18, name: ' Hulk' },
+  { id: 19, name: ' Thor' },
+  { id: 20, name: ' Spider-Man' }
+];
