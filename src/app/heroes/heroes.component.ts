c4c3ee4ad47b3512c45aa578e26e5e55ce73ade8
rudycito23 <rrlucas@live.com>
2024-01-23 19:41:37 -0600

    Added comments to understand each line of code better

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,10 +7,10 @@
   /*  */
 }
 import { Component } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common'; // provides common directive, in this case pipes
+import { CommonModule, NgFor } from '@angular/common'; // provides common directive and pipes
 import { FormsModule } from '@angular/forms'; // provides support for template-driven forms: ngModel
 import { Hero } from '../hero'; // importing the Hero interface from hero.ts
-import { HEROES } from '../mock-heroes';
+import { HEROES } from '../mock-heroes';  // importing the HEROES array from mock-heroes.ts
 
 // Component decorator providing metadata for the HeroesComponent
 @Component({
@@ -28,10 +28,11 @@ export class HeroesComponent {
   };
 */
 
-  //  expose the HEROES array for binding
+  //  expose the HEROES array for binding in the component's template
   heroes = HEROES;
 
-  selectedHero?: Hero;
+  selectedHero?: Hero;  // property to hold the currently selected hero; type = Hero
+  //  method to handle the selection of a hero; it sets the selectedHero property to the hero passed as an argument
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
