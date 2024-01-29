d4066a91ab632782b9b837388ef7c9cc36e47674
rudycito23 <rrlucas@live.com>
2024-01-29 09:45:26 -0600

    Refactored data access to the HeroService class; registered the HeroService as the provider of its service at the root level so that is can be injected anywhere in the app; used Angular Dependency Injection to inject it into a component; gave HeroService get data method an asynchronous signature; discovered Observable and the RxJS Observable library; used RxJS of() to return Observable<Hero[]>, an observable of mock heroes; the component's ngOnInit lifecycle hook calls the HeroService method, not the constructor; create a MessageService for loosely coupled communication between classes; the HeroService injected into a component is created with another injected service, MessageService

---
+++ src/app/services/hero.service.ts
@@ -0,0 +1,27 @@
+/* Inject the MessageService into the HeroService which is injected into the HeroesComponent;
+    this is know as service-in-service scenario */
+
+/*  */
+
+import { Injectable } from '@angular/core'; //  importing Injectable decorator
+import { Hero } from '../hero'; //  importing Hero interface
+import { HEROES } from '../mock-heroes';  //  Importing the HEROES array which contains mock data
+import { Observable, of } from 'rxjs';  //  importing Observable and of from rxjs, which is used for reactive programming
+import { MessageService } from './message.service'; //  importing MessageService, a service for handling messages
+
+//  Injectable decorator marks this class as available to be provided and injected as a dependency
+@Injectable({
+  providedIn: 'root'  //  indicates that this service should be provided in the root injector
+})
+export class HeroService {
+
+  //  constructor to inject MessageService
+  constructor(private MessageService: MessageService) { }
+
+  //  getHeroes method returns an Observable of Hero array
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);  //  wraps HEROES in an Observable
+    this.MessageService.add('HeroService: fetched heroes');
+    return heroes;  //  returns the Observable
+  }
+}
